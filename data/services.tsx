1dc678da1f59f657839c3ce971c14b26561ef81b
rezaputrah <128364238+rezaputrah@users.noreply.github.com>
2023-03-20 17:21:23 +0700

    Update services.tsx

--- data/services.tsx
+++ data/services.tsx
@@ -2,31 +2,31 @@ export const services = [
   {
     icon: <div></div>,
     title: "Web design",
-    text: "Those who do not know how to pursue.",
+    text: "Kami memahami bahwa setiap orang memiliki kebutuhan yang berbeda-beda sehingga kami memastikan sesuai dengan keinginan setiap customer kami.",
   },
   {
     icon: <div></div>,
-    title: "Development",
-    text: "Due to its widespread use as filler text for layouts, non-readability is of great importance.",
+    title: "User Friendly",
+    text: "Pengalaman menggunakan website yang mudah dan enak diliat membuat pelanggan akan senang untuk terus berkunjung dan berbelanja di website anda dan kami akan hal tersebut.",
   },
   {
     icon: <div></div>,
-    title: "Branding",
-    text: "There are many variations of passages of available, but the majority alteration in some form.",
+    title: "Affordable Price",
+    text: "Tidak perlu lagi berfikir dua kali lagi untuk mendigitalisasi usaha kalian karna khawatir dengan biaya pembuatan website yang mahal, kami hadir dengan harga terjangkau untuk membantu kalian.",
   },
   {
     icon: <div></div>,
-    title: "Easy to costumize",
-    text: "The generated Lorem Ipsum is therefore always free from repetition, injected humour.",
+    title: "Newest Technology",
+    text: "Teknologi pembuatan website terus berkembang, Rasakan pengalaman dengan teknologi terbaru yang selalu kami gunakan untuk memberikan layanan terbaik untuk Anda. Dengan teknologi terbaru yang kami miliki, kami dapat memberikan solusi yang lebih inovatif, efektif, dan efisien untuk memenuhi kebutuhan Anda .",
   },
   {
     icon: <div></div>,
-    title: "Unrivaled Quality",
-    text: "The generated Lorem Ipsum is therefore always free from repetition, injected humour.",
+    title: "Ads Expert",
+    text: "Pengalaman kami membantu pelanggan untuk mengatur website mereka dan menjadikan website mereka terlihat dan banyak dikunjungi meningkatkan konversi penjualan.",
   },
   {
     icon: <div></div>,
-    title: "Online Marketing",
-    text: "This is required when, for example, the is not yet available. Dummy text is also known as 'fill text'. ",
+    title: "Professional Team",
+    text: "Staff kami yang berpengalaman dalam bidangnya akan memberikan hasil terbaik pada website yang diingkan oleh setiap pelanggan kami. ",
   },
 ]
