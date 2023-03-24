6d8690c99656d301d8414bac9bb787e4452c8ed5
alammooo <abdullah.alamm@gmail.com>
2023-03-24 11:49:54 +0700

    add icons on service

--- components/Services.tsx
+++ components/Services.tsx
@@ -1,21 +1,30 @@
 import { services } from "@/data/services"
 import BlueBorder from "./BlueBorder"
 import SectionHead from "./SectionHead"
+import Image from "next/image"
 
 export default function Services() {
   return (
-    <section className="mx-auto max-w-6xl py-24 text-center mt-52 md:mt-0" id="Services">
+    <section
+      className="mx-auto mt-52 max-w-6xl py-24 text-center md:mt-0"
+      id="Services">
       <SectionHead type="services" />
-      <div className="mt-10 grid md:grid-cols-3 gap-3">
+      <div className="mt-10 grid gap-3 md:grid-cols-3">
         {services.map((e, i) => (
           <div
             key={i}
             className={`${
-              i % 2 !== 0 ? "border border-slate-50 shadow-lg" : ""
-            } flex flex-col gap-5 rounded py-10 px-7 hover:bg-sky-50`}>
-            <h1 className="text-xl font-medium">{e.title}</h1>
+              i % 2 !== 0 ? "border border-zinc-100 shadow-lg" : ""
+            } flex flex-col gap-6 rounded py-8 px-7 hover:bg-sky-50`}>
+            <Image
+              src={e.icon}
+              width={60}
+              alt={e.title}
+              className="self-center"
+            />
+            <h1 className="text-lg font-medium">{e.title}</h1>
             <BlueBorder />
-            <p className="px-3 text-sm leading-6 text-slate-400">{e.text}</p>
+            <p className="px-3 text-sm leading-6 text-zinc-400">{e.text}</p>
           </div>
         ))}
       </div>
--- data/services.tsx
+++ data/services.tsx
@@ -1,31 +1,38 @@
+import Ads from "../assets/icons/ads2.png"
+import Tech from "../assets/icons/technology.png"
+import ResponsiveDesign from "../assets/icons/responsive-design.png"
+import WebCust from "../assets/icons/web.png"
+import Price from "../assets/icons/budget.png"
+import ProTech from "../assets/icons/engineer.png"
+
 export const services = [
   {
-    icon: <div></div>,
+    icon: WebCust,
     title: " Custom Web design",
     text: "Kami memahami bahwa setiap orang memiliki kebutuhan yang berbeda-beda, sehingga kami memastikan apa yang kami kerjakan sesuai dengan keinginan pelanggan.",
   },
   {
-    icon: <div></div>,
+    icon: ResponsiveDesign,
     title: "User Friendly",
     text: "Pengalaman menggunakan website dengan mudah dan nyaman digunakan membuat pelanggan senang untuk terus berkunjung, dan kami mengerti akan hal itu.",
   },
   {
-    icon: <div></div>,
+    icon: Price,
     title: "Affordable Price",
     text: "Tidak perlu lagi berfikir dua kali untuk mendigitalisasi usaha anda, kami hadir dengan harga terjangkau untuk membantu anda mewujudkan website impian.",
   },
   {
-    icon: <div></div>,
+    icon: Tech,
     title: "Newest Technology",
     text: "Dengan teknologi terbaru yang kami miliki, kami dapat memberikan solusi yang lebih inovatif, efektif, dan efisien untuk memenuhi kebutuhan Anda .",
   },
   {
-    icon: <div></div>,
+    icon: Ads,
     title: "Ads Expert",
     text: "Pengalaman kami membantu pelanggan untuk mengatur websitenya dan menjadikan website anda terlihat dan banyak dikunjungi sehingga meningkatkan konversi penjualan usaha.",
   },
   {
-    icon: <div></div>,
+    icon: ProTech,
     title: "Professional Team",
     text: "Staff kami yang berpengalaman dalam bidangnya akan memberikan hasil terbaik sesuai yang diinginkan oleh setiap pelanggan kami. ",
   },
