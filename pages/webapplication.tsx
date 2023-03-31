b2329bd1ccdf315df978c51aa3ebf3178278ca3f
alammooo <abdullah.alamm@gmail.com>
2023-03-31 21:12:13 +0700

    add navbar style

--- pages/webapplication.tsx
+++ pages/webapplication.tsx
@@ -10,7 +10,7 @@ export default function webapplication() {
       <Head>
         <title>Entitag | Application Development</title>
       </Head>
-      <section className="md:my-24 pt-10">
+      <section className="md:my-24 pt-10 md:pt-0">
         <div
           className={`mx-auto flex max-w-6xl flex-col items-center gap-5 md:flex-row`}>
           <div className="p-5 md:p-0">
